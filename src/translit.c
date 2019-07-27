4b43b54198bcbe2cd0e4c90c7fa295eaa215de2d
Urs-Jakob <ujr@bernina>
2019-07-27 18:07:11 +0200

    Amend translit command (step 3: complement of src)

--- src/translit.c
+++ src/translit.c
@@ -1,7 +1,9 @@
 
 #include "common.h"
 
-static int parseargs(int argc, char **argv, const char **src, const char **dst);
+static int parseargs(int argc, char **argv,
+  bool *allbut, const char **src, const char **dst);
+static int xindex(bool allbut, const char *s, int c, int lastdst);
 static int index(const char *s, int c);
 static void usage(const char *msg);
 
@@ -9,12 +11,13 @@ int
 translitcmd(int argc, char **argv)
 {
   int c, i, r;
-  const char *src, *dst;
+  bool allbut = false;
+  const char *src = "";
+  const char *dst = "";
 
   SHIFTARGS(argc, argv, 1);
-  r = parseargs(argc, argv, &src, &dst);
+  r = parseargs(argc, argv, &allbut, &src, &dst);
   if (r < 0) return FAILHARD;
-  SHIFTARGS(argc, argv, r);
 
   int dstlen = strlen(dst);
   int lastdst = dstlen - 1;
@@ -24,15 +27,15 @@ translitcmd(int argc, char **argv)
 
   if (drop) {
     while ((c = getch()) != EOF) {
-      i = index(src, c);
+      i = xindex(allbut, src, c, lastdst);
       if (i < 0) putch(c); // copy; else: drop
     }
   }
   else do {
-    i = index(src, c = getch());
+    i = xindex(allbut, src, c = getch(), lastdst);
     if (squash && i >= lastdst) { // squash
       putch(dst[lastdst]); // translate first
-      do i = index(src, c = getch());
+      do i = xindex(allbut, src, c = getch(), lastdst);
       while (i >= lastdst); // drop remaining
     }
     if (c != EOF) {
@@ -45,20 +48,57 @@ translitcmd(int argc, char **argv)
 }
 
 static int
-parseargs(int argc, char **argv, const char **src, const char **dest)
+parseargs(int argc, char **argv,
+  bool *allbut, const char **src, const char **dst)
 {
-  if (argc < 1 || argc > 2) {
-    usage("expect one or two arguments");
+  int i;
+  for (i = 0; i < argc && argv[i]; i++) {
+    const char *p = argv[i];
+    if (*p != '-' || streq(p, "-")) break; // no more option args
+    if (streq(p, "--")) { ++i; break; } // end of option args
+    for (++p; *p; p++) {
+      switch (*p) {
+        case 'c': *allbut = true; break;
+        case 'h': usage(0); break;
+        default: usage("invalid option"); return -1;
+      }
+    }
+  }
+
+  /* the src argument is mandatory */
+  if (i < argc && argv[i]) {
+    *src = argv[i++];
+  }
+  else {
+    usage("the src argument is missing");
     return -1;
   }
 
-  *src = argv[0];
-  *dest = argc > 1 ? argv[1] : "";
+  /* the dst argument is optional */
+  if (i < argc && argv[i]) {
+    *dst = argv[i++];
+  }
+
+  /* expect no more arguments */
+  if (i < argc && argv[i]) {
+    usage("too many arguments");
+    return -1;
+  }
 
   return argc;
 }
 
-/* index of c in s, -1 if not found */
+/* xindex: conditionally invert result from index */
+static int
+xindex(bool allbut, const char *s, int c, int lastdst)
+{
+  if (c == EOF) return -1;
+  if (!allbut) return index(s, c);
+  if (index(s, c) >= 0) return -1;
+  return lastdst+1; // request squashing
+}
+
+/* index: return position of c in s, -1 if not found */
 static int
 index(const char *s, int c)
 {
@@ -67,12 +107,13 @@ index(const char *s, int c)
   return s[i] == c ? i : -1;
 }
 
-/* print usage and exit */
+/* usage: print usage and exit */
 static void
-usage(const char *msg)
+usage(const char *errmsg)
 {
-  FILE *fp = msg ? stderr : stdout;
-  if (msg) fprintf(fp, "%s: %s\n", me, msg);
-  fprintf(fp, "Usage: %s src [dest]\n", me);
-  exit( msg ? FAILHARD : SUCCESS);
+  FILE *fp = errmsg ? stderr : stdout;
+  if (errmsg) fprintf(fp, "%s: %s\n", me, errmsg);
+  fprintf(fp, "Usage: %s [-c] src [dest]\n", me);
+  fprintf(fp, "Transliterate, squash, or delete characters\n");
+  exit(errmsg ? FAILHARD : SUCCESS);
 }
