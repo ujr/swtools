b82a867865b2c45045f9b229c613c6441469a4df
Urs-Jakob <ujr@bernina>
2020-09-16 23:40:21 +0200

    Implement option -n (permute 1..num) for shuffle

--- src/shuffle.c
+++ src/shuffle.c
@@ -8,8 +8,12 @@
 #include "common.h"
 #include "lines.h"
 
+static void nomem() { longjmp(errjmp, 1); }
+#define BUF_ABORT nomem()
+#include "buf.h"
+
 static void shuffle(size_t v[], size_t n);
-static int parseopts(int argc, char **argv, long *seed);
+static int parseopts(int argc, char **argv, long *seed, int *num);
 static void usage(const char *errmsg);
 
 int
@@ -18,9 +22,10 @@ shufflecmd(int argc, char **argv)
   int r;
   size_t n;
   long seed = -1;
+  int num = -1;
   struct lines lines = { 0, 0, 0 }; // must zero-init for buf.h
 
-  r = parseopts(argc, argv, &seed);
+  r = parseopts(argc, argv, &seed, &num);
   if (r < 0) return FAILHARD;
   SHIFTARGS(argc, argv, r);
 
@@ -28,6 +33,17 @@ shufflecmd(int argc, char **argv)
 
   if (verbosity > 0 && seed >= 0) {
     fprintf(stderr, "(random seed = %ld, RAND_MAX = %ld)\n", seed, (long) RAND_MAX);
+  }
+
+  if (num > 0) {
+    size_t *nums = 0;
+    for (int i = 1; i <= num; i++)
+      buf_push(nums, i);
+    shuffle(nums, (size_t) num);
+    for (int i = 0; i < num; i++)
+      printf("%zd ", nums[i]);
+    printf("\n");
+    return SUCCESS;
   }
 
   if (argc == 0 || !*argv) {
@@ -77,7 +93,7 @@ static void shuffle(size_t v[], size_t n)
 /* Options and usage */
 
 static int
-parseopts(int argc, char **argv, long *seed)
+parseopts(int argc, char **argv, long *seed, int *num)
 {
   long l;
   int i, showhelp = 0;
@@ -88,6 +104,14 @@ parseopts(int argc, char **argv, long *seed)
     if (streq(p, "--")) { ++i; break; } /* end of option args */
     for (++p; *p; p++) {
       switch (*p) {
+        case 'n':
+          if (argv[i+1] && (l = atoi(argv[i+1])) > 0 && !*(p+1)) {
+            *num = (int) l;
+            i += 1;
+            break;
+          }
+          usage("option -n requires a positive number argument");
+          return -1;
         case 's': 
           if (argv[i+1] && (l = atol(argv[i+1])) >= 0 && !*(p+1)) {
             *seed = l;
@@ -116,7 +140,9 @@ static void usage(const char *errmsg)
 {
   FILE *fp = errmsg ? stderr : stdout;
   if (errmsg) fprintf(fp, "%s: %s\n", me, errmsg);
-  fprintf(fp, "Usage: %s [-s seed]\n", me);
+  fprintf(fp, "Usage: %s [-n num] [-s seed] [files]\n", me);
   fprintf(fp, "Shuffle text lines (random permutation)\n");
   fprintf(fp, "  -s seed   seed value for random number generator\n");
+  fprintf(fp, "  -n num    random permutation of the numbers 1..num\n");
+  fprintf(fp, "With option -n, input files are ignored\n");
 }
